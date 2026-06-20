747b9febe0656e50fb6cba67801925e5000c4b50
Henry M. Leme <henrymleme1000@gmail.com>
2026-06-20 20:12:18 -0300

    highlight de movimento possivel fix

--- src/application/UI.java
+++ src/application/UI.java
@@ -18,9 +18,8 @@ public class UI {
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
-    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
 
     // Método para limpar a tela [cite: 111]
     public static void clearScreen() {
@@ -62,7 +61,18 @@ public class UI {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j]);
+                printPiece(pieces[i][j], false);
+            }
+            System.out.println();
+        }
+        System.out.println("  a  b  c d  e f  g h");
+    }
+
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < pieces.length; j++) {
+                printPiece(pieces[i][j], possibleMoves[i][j]);
             }
             System.out.println();
         }
@@ -70,7 +80,10 @@ public class UI {
     }
 
     // Método auxiliar para colorir a peça [cite: 97]
-    private static void printPiece(ChessPiece piece) {
+    private static void printPiece(ChessPiece piece, boolean background) {
+        if (background) {
+            System.out.print(ANSI_YELLOW_BACKGROUND);
+        }
         if (piece == null) {
             System.out.print(ANSI_INVIS + "▰" + ANSI_RESET);
         } else {
